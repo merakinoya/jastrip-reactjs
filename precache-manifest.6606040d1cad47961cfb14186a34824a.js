6a40bc172522a7b4571bbdc03332b2e57f7c36c4
Ilham Anasruloh <ilham.anasruloh@student.uny.ac.id>
2020-07-18 23:43:49 +0700

    Updates

---
+++ precache-manifest.6606040d1cad47961cfb14186a34824a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80aae851674e22a61706821f2591b8b0",
+    "url": "/Coba-ReactWebApp/index.html"
+  },
+  {
+    "revision": "6c465e98ddce476a7cbe",
+    "url": "/Coba-ReactWebApp/static/css/main.fed17ab8.chunk.css"
+  },
+  {
+    "revision": "d7c2e437a5a2b04c7dfb",
+    "url": "/Coba-ReactWebApp/static/js/2.ab766650.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Coba-ReactWebApp/static/js/2.ab766650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c465e98ddce476a7cbe",
+    "url": "/Coba-ReactWebApp/static/js/main.7ccc03b7.chunk.js"
+  },
+  {
+    "revision": "9b60c2ec51bcd101f30e",
+    "url": "/Coba-ReactWebApp/static/js/runtime-main.27fd8110.js"
+  },
+  {
+    "revision": "929e6e342828ae9198235aa1e8d41cb5",
+    "url": "/Coba-ReactWebApp/static/media/brandlogo.929e6e34.svg"
+  }
+]);
