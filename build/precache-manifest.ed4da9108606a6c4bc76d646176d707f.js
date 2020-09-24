ac255e9862431cf180b310defb3c7423e2c4ca41
Ilham Anasruloh <ilham.anasruloh@student.uny.ac.id>
2020-09-24 19:56:47 +0700

    Add .gitignore for node_modules

---
+++ build/precache-manifest.ed4da9108606a6c4bc76d646176d707f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27365df1722dd2dcbbe96ee9bc8f358c",
+    "url": "/jastrip-reactjs/index.html"
+  },
+  {
+    "revision": "15aaed9bbf94f95328a3",
+    "url": "/jastrip-reactjs/static/css/main.24916592.chunk.css"
+  },
+  {
+    "revision": "7fbb8a23ee50751c5c19",
+    "url": "/jastrip-reactjs/static/js/2.cdbb0572.chunk.js"
+  },
+  {
+    "revision": "2415e6967177769184874497a3c934bc",
+    "url": "/jastrip-reactjs/static/js/2.cdbb0572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15aaed9bbf94f95328a3",
+    "url": "/jastrip-reactjs/static/js/main.6fd24675.chunk.js"
+  },
+  {
+    "revision": "3ec7fa902629d506875a",
+    "url": "/jastrip-reactjs/static/js/runtime-main.f6e655da.js"
+  },
+  {
+    "revision": "929e6e342828ae9198235aa1e8d41cb5",
+    "url": "/jastrip-reactjs/static/media/brandlogo.929e6e34.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/jastrip-reactjs/static/media/x.b0200bf7.svg"
+  }
+]);
